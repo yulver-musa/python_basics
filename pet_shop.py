9440f5ebbcff1461920810eb197c77696de7ed5a
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2024-11-22 20:39:53 +0200

    Add files via upload

---
+++ pet_shop.py
@@ -0,0 +1,6 @@
+number_of_dog_food_pkt = int(input())
+number_of_cat_food_pkt = int(input())
+a = number_of_dog_food_pkt * 2.50
+b = number_of_cat_food_pkt * 4.00
+total_sum = a + b
+print(f'{total_sum} lv.')
