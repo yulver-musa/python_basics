8cf7fe09b3707edc0586e12f090fb6e6019c5a5f
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2024-11-22 20:40:37 +0200

    Add files via upload

---
+++ projects_creation.py
@@ -0,0 +1,5 @@
+name_of_architect = input()
+number_of_project = int(input())
+completion_time = number_of_project * 3
+print(f'The architect {name_of_architect} will need {completion_time} hours '
+      f'to complete {number_of_project} project/s.')
